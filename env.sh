7c6672965bf26fb98186791da67486f4d6a23fc3
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2024-08-21 15:43:56 -0400

    Added config file

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-export DSA_URL='https://athena.rc.ufl.edu/api/v1'
+export DSA_URL='http://ec2-3-230-122-132.compute-1.amazonaws.com:8080/api/v1/'
 export DSA_USER='fusionguest'
 export DSA_PWORD='Fus3yWasHere'
